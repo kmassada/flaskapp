b8395b6a7c2a723c2032cb389446862c4b18051c
Kenneth Massada <kmassada@gmail.com>
2017-01-10 16:03:06 -0800

    adding basic routes, mysql config, and templates

---
+++ app.py
@@ -0,0 +1,36 @@
+"""
+simple flask app
+"""
+
+import os
+from flask import Flask, render_template
+from flask_mysqldb import MySQL
+
+# App Configurations
+app = Flask(__name__)
+filedir = os.path.dirname(__file__)
+mysql = MySQL()
+
+# MySQL configurations
+app.config['CLOUDSQL_CONNECTION_NAME'] = os.environ.get('CLOUDSQL_CONNECTION_NAME')
+app.config['CLOUDSQL_USER'] = os.environ.get('CLOUDSQL_USER')
+app.config['CLOUDSQL_PASSWORD'] = os.environ.get('CLOUDSQL_PASSWORD')
+app.config['CLOUDSQL_DATABASE'] = os.environ.get('CLOUDSQL_DATABASE')
+mysql.init_app(app)
+
+
+def connect_to_cloudsql():
+    """connect_to_cloudsql"""
+    if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
+        return True
+    else:
+        return False
+
+@app.route("/")
+def main():
+    """here"""
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
